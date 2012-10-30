6c468a4634bf97440d05695901b1fc9e3fd025af
Jessica <jess@jkwok>
2012-10-30 14:39:53 -0400

    removed proprietary information, hah

--- api/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Database setup
-define('HOST', 'localhost');
-define('DB', 'agrowcul_main'); //agrowcul_main
-define('USER', 'agrowcul_evan'); //agrowcul_evan
-define('PW', 'darkowen'); //darkowen
-define('MC_APIKEY', '20ae3526370ac08650a3f6945258d807-us2');
-?>
